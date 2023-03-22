e57ac7107fde30b0588fffae30b6b4f247a9c2ca
kiyoyoneko <kiyoedm@gmail.com>
2023-03-22 05:49:37 -0500

    coconuts

--- trader.py
+++ trader.py
@@ -1,11 +1,13 @@
 from typing import Dict, List
 from datamodel import OrderDepth, TradingState, Order
 import numpy as np
-mid_banana = [5000] #high score 591
+mid_banana = [] #high score 591
 mid_pearl = [] #high score 852
+mid_coco = []
 class Trader:
     profit = 0
     limit = 20
+    coco_limit = 600
     banana_bal = 0
     ema = 5000
     def run(self, state: TradingState) -> Dict[str, List[Order]]:
@@ -13,6 +15,7 @@ class Trader:
         result = {}
         pearl_position = state.position.get('PEARLS', 0)
         banana_position = state.position.get('BANANAS', 0)
+        coco_position = state.position.get('COCONUTS', 0)
         for product in state.order_depths.keys():
             if product == 'PEARLS':
                 order_depth: OrderDepth = state.order_depths[product]
@@ -85,7 +88,8 @@ class Trader:
                     print('buy')
                 else:
                     print('sell')
-                factor = int(0.5*spread)
+                factor = int(0.1*spread)
+                print('factor:',factor)
                 if state.timestamp<600:
                     break
                 if asks[0]['price'] < acceptable_price or (asks[0]['price'] == acceptable_price and banana_position < -2):
@@ -103,16 +107,71 @@ class Trader:
                     banana_position -= bids[0]['vol']
                     print("SELL", str(bids[0]['vol']) + "x", bids[0]['price'], 'banana_positions:',
                         banana_position)
-                elif banana_position > 15:
-                    orders.append(Order(product, acceptable_price+factor/2, -10))
-                elif banana_position < -15:
-                    orders.append(Order(product, acceptable_price-factor/2, 10))
                 elif banana_position > 0:
                     # orders.append(Order(product, 10002, -(Trader.limit-pearl_position)))
-                    orders.append(Order(product, acceptable_price+factor, -10))
-                    orders.append(Order(product, acceptable_price-factor, 5))
+                    orders.append(Order(product, acceptable_price+factor, -18))
+                    orders.append(Order(product, acceptable_price-factor, 9))
                 else:
-                    orders.append(Order(product, acceptable_price+factor, -5))
-                    orders.append(Order(product, acceptable_price-factor, 10))
+                    orders.append(Order(product, acceptable_price+factor, -9))
+                    orders.append(Order(product, acceptable_price-factor, 18))
+                result[product] = orders
+                """elif banana_position > 10 or not buy:
+                    orders.append(Order(product, acceptable_price+factor/2, -10))
+                    orders.append(Order(product, acceptable_price - factor, 5))
+                elif banana_position < -10:
+                    orders.append(Order(product, acceptable_price + factor, -5))
+                    orders.append(Order(product, acceptable_price-factor/2, 10))"""
+            if product == 'COCONUTS':
+                order_depth: OrderDepth = state.order_depths[product]
+                if len(order_depth.sell_orders) > 0 and len(order_depth.buy_orders) > 0:
+                    mid_coco.append((max(order_depth.buy_orders.keys())+min(order_depth.sell_orders.keys()))/2)
+                orders: list[Order] = []
+                asks = []
+                bids = []
+                spread = min(order_depth.sell_orders.keys()) - max(order_depth.buy_orders.keys())
+                while len(order_depth.sell_orders) > 0:
+                    asks.append({'price': min(order_depth.sell_orders.keys()),
+                                'vol': order_depth.sell_orders[min(order_depth.sell_orders.keys())]})
+                    del order_depth.sell_orders[min(order_depth.sell_orders.keys())]
+                while len(order_depth.buy_orders) != 0:
+                    bids.append({'price': max(order_depth.buy_orders.keys()),
+                                'vol': order_depth.buy_orders[max(order_depth.buy_orders.keys())]})
+                    del order_depth.buy_orders[max(order_depth.buy_orders.keys())]
+                avg_window = 20
+                acceptable_price = np.mean(mid_coco[-avg_window:])
+                a10 = np.mean(mid_coco[-50:])
+                a100 = np.mean(mid_coco[-200:])
+                std = np.std(mid_coco[-avg_window:])
+                buy = (a10 - a100) >0
+                if buy:
+                    print('buy')
+                else:
+                    print('sell')
+                factor = int(0.1*spread)
+                print('factor:',factor)
+                if state.timestamp<600:
+                    break
+                if asks[0]['price'] < acceptable_price or (asks[0]['price'] == acceptable_price and coco_position < -2):
+                    if coco_position - asks[0]['vol'] > Trader.limit:
+                        asks[0]['vol'] = -(Trader.limit - coco_position)
+                        print('missed full trade')
+                    orders.append(Order(product, asks[0]['price'], -asks[0]['vol']))
+                    coco_position -= asks[0]['vol']
+                    print("BUY", str(-asks[0]['vol']) + "x", asks[0]['price'], 'coco_positions:',coco_position)
+                elif bids[0]['price'] > acceptable_price or (bids[0]['price'] == acceptable_price and coco_position > -5):
+                    if coco_position - bids[0]['vol'] < -Trader.limit:
+                        bids[0]['vol'] = coco_position + Trader.limit
+                        print('missed full trade')
+                    orders.append(Order(product, bids[0]['price'], -bids[0]['vol']))
+                    coco_position -= bids[0]['vol']
+                    print("SELL", str(bids[0]['vol']) + "x", bids[0]['price'], 'coco_positions:',
+                        coco_position)
+                """elif coco_position > 0:
+                    # orders.append(Order(product, 10002, -(Trader.limit-pearl_position)))
+                    orders.append(Order(product, acceptable_price+factor, -18))
+                    orders.append(Order(product, acceptable_price-factor, 9))
+                else:
+                    orders.append(Order(product, acceptable_price+factor, -9))
+                    orders.append(Order(product, acceptable_price-factor, 18))"""
                 result[product] = orders
         return result
